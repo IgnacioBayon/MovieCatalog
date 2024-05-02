fa1f4bd30e95e2f9d915c29ca7944159d2771fdd
ibayonju <202105547@alu.comillas.edu>
2024-05-02 12:26:44 +0200

    add ratings

--- api/films/serializers.py
+++ api/films/serializers.py
@@ -8,12 +8,7 @@ class FilmSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Film
-        fields = ["title", "genre", "description", "director", "release_date", "rating"]
-
-    def validate_rating(self, value):
-        if value < 1 or value > 5:
-            raise exceptions.ValidationError("Rating must be between 1 and 5")
-        return value
+        fields = ["title", "genre", "description", "director", "release_date"]
 
     def create(self, validated_data):
         return models.Film.objects.create(**validated_data)
